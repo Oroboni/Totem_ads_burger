3f2548b0035ac071edfe72a066622799d38befda
Matheus Cuero <101690604+Oroboni@users.noreply.github.com>
2025-04-07 14:29:03 -0300

    um pouco mais de css

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf234e17aa2972f07635d9323fd4f01ed3f4f094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c85d9063aa1fd3909ca725a30f00d9ad5db614")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
