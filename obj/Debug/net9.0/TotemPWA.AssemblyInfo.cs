98ec37c9a7afea8074d12aff0ba967f243357457
Luís Felipe Glauser Lucas <102819769+LuisGlauser@users.noreply.github.com>
2025-04-14 16:04:49 -0300

    update: Atualizando css da tela editar

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d45bec6412517a3016f7d98c282620ae95893e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+569152f84890611bc372a88be8e395d8b0a7b72d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
