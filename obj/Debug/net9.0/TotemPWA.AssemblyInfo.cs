0297e65c4a663676eebc0780055c7fb8a400cd82
Matheus Cuero <101690604+Oroboni@users.noreply.github.com>
2025-05-19 14:30:05 -0300

    sqlite

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f008d83499cabbff16e80f8a570070099832bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44e8a6e197607d32c3c6bcce172457ec46cb5eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
