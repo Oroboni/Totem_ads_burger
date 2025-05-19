17b2880b138f5886f68f9a5e44c2f7975cf380ff
Elisa Prestes <bruna.elisa.prestes@gmail.com>
2025-05-19 16:31:58 -0300

    Mudanças para ficar mais leal ao figma

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0be3ae85ef18ae5bcc9b4af6dfcb07021343d938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b34759cd23f385e3c7f64beece4d4ec5d78f9fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
